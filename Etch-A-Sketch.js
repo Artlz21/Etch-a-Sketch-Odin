fd21fae07f6e15ac975b704b6b4f17290bc1c290
Artlz21 <darren.lozano01@utrgv.edu>
2023-06-07 16:53:23 -0500

    Project finished more detail and color could be added

--- Etch-A-Sketch.js
+++ Etch-A-Sketch.js
@@ -1,22 +1,4 @@
-const squares = () => {
-    let grid = document.querySelector(".grid");
-    let square = document.querySelector(".grid .square");
-    grid.removeChild(square);
-    for(j = 0; j < 16; j++){
-        for(let i = 0; i < 16; i++){
-            let newClone = square.cloneNode(true);
-            newClone.addEventListener("mouseenter", (e) => {e.target.style.backgroundColor = "black"});
-            grid.appendChild(newClone);
-
-            // if((i % 2) == 1)
-            // {
-            //     newClone.style.backgroundColor = "lightcoral"
-            // }
-        }
-    }
-    
-}
-
+// Gives function to the clear button to clear grid of filled squares
 const clearButton = () => {
     let square = document.getElementsByClassName("square");
     let clrButton = document.querySelector(".Clear-button");
@@ -28,8 +10,43 @@ const clearButton = () => {
     )
 }
 
+// Generates copies of a square and appends to the grid on screen (needs to recieve length of side)
+const squares = (size, square) => {
+    // Select the grid and square on the page 
+    let grid = document.querySelector(".grid");
+
+    square.style.width = `${(1/size) * 100}%`;
+    square.style.height = `${(1/size) * 100}%`;
+
+    // Loop through and clone the template then appends the clone to the grid
+    for(j = 0; j < size; j++){
+        for(let i = 0; i < size; i++){
+            let newClone = square.cloneNode(true);
+            newClone.addEventListener("mouseenter", (e) => {e.target.style.backgroundColor = "black"});
+            grid.appendChild(newClone);
+        }
+    }
+}
+
+// Get value from slider id="Input-range"
+const sliderInput = () => {
+    let sliderValue = document.getElementById("Input-range").value;
+    let square = document.querySelector(".grid .square");
+    let cloneSquare = square.cloneNode(true)
+    console.log(sliderValue)
+    let grid = document.querySelector(".grid");
+    grid.innerHTML = "";
+
+    squares(sliderValue, cloneSquare);
+}
+
 (() => {
-    squares();
+    // Add event to slider
+    let sliderEvent = document.getElementById("Input-range");
+    sliderEvent.addEventListener("input", sliderInput);
+
+    // 
+    sliderInput();
     clearButton();
 })
 ();
